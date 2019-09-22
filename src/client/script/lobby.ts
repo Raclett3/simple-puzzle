10e2e6c6adfe0c65d8a4998bdaf312d2b92d244c
Raclette <im@s283.net>
2019-09-22 21:55:03 +0900

    Client: Make the lobby to send messages
    The lobby now sends messages when the button is clicked

--- src/client/script/lobby.ts
+++ src/client/script/lobby.ts
@@ -1,8 +1,11 @@
 import {wrapper} from "./index"
+import {send} from "./websocket";
 
 const lobby = document.createElement("div");
 
 export function init() {
+    remove();
+
     const matchName = document.createElement("input");
     const create = document.createElement("input");
     const join = document.createElement("input");
@@ -13,6 +16,20 @@ export function init() {
     join.setAttribute("type", "button");
     create.setAttribute("value", "作成");
     join.setAttribute("value", "参加");
+
+    create.addEventListener("click", function() {
+        send({
+            type: "CREATE",
+            name: matchName.value
+        });
+    });
+
+    join.addEventListener("click", function() {
+        send({
+            type: "JOIN",
+            name: matchName.value
+        });
+    });
 
     lobby.appendChild(matchName);
     lobby.appendChild(create);
